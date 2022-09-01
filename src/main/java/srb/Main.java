0984f3b7298fe5281a6876ddbcca893f66094a0e
Sandro Roger Boschetti <sandro.boschetti@ebserh.gov.br>
2022-09-01 17:19:22 -0300

    Implementado página de envio. Testando...

--- src/main/java/srb/Main.java
+++ src/main/java/srb/Main.java
@@ -6,11 +6,7 @@ import srb.sendmsg.SendMsg;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		System.out.println(System.getenv("SRB"));
-		
-		System.out.println("Executando o main() de dentro de Main.");
-		
+
 //		SendMsg.execute();
 //		ReadExcel.execute();
 
