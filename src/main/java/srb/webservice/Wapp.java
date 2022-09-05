25ba644a91abc7c401405b9b8ace4e61623e8815
Sandro Roger Boschetti <sandro.boschetti@ebserh.gov.br>
2022-09-05 17:20:23 -0300

    Teste: copia mensagem enviada.

--- src/main/java/srb/webservice/Wapp.java
+++ src/main/java/srb/webservice/Wapp.java
@@ -83,8 +83,9 @@ public class Wapp {
 		
 		//Código temporário para controle e testes: envio de cópia da mensagem enviada para um telefone específico
 		Date date = new Date();
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy (HH:mm)");
-		sdf.setTimeZone(TimeZone.getTimeZone("America/São Paulo"));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy (HH:mm:ss)");
+		//Quando horário local é 17:13 o horário da data é 20:13. Pus o setTimeZone que não está funcionando.
+		sdf.setTimeZone(TimeZone.getTimeZone("America/Sao Paulo"));
 		String dt = sdf.format(date);
 //		System.out.println("Upload do arquivo feito em: " + dt);
 		MsgModel srbMsgModel = new MsgModel();
