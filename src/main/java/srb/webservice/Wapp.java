d8991a0d14e87857d7b3a1ada536cfa87670e3e7
Sandro Roger Boschetti <sandro.boschetti@ebserh.gov.br>
2022-09-05 18:15:20 -0300

    Inclusão de código de teste no endpoint /wapp

--- src/main/java/srb/webservice/Wapp.java
+++ src/main/java/srb/webservice/Wapp.java
@@ -46,7 +46,7 @@ public class Wapp {
 		String dt = sdf.format(date);
 		MsgModel srbMsgModel = new MsgModel();
 		srbMsgModel.setPhoneNumberFrom(From); //+14155238886
-		srbMsgModel.setPhoneNumberTo("+553183349238");
+		srbMsgModel.setPhoneNumberTo("whatsapp:+553183349238");
 		String srbMessage = "Em " + dt + " " + From + " enviou uma requisição (mensagem qualquer) para o endpoint /wapp.";
 		srbMsgModel.setMessage(srbMessage);
 		String msg = enviaMensagem(srbMsgModel);
