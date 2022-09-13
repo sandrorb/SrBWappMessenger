ce634df22fac3c4d0a582ec56f4c4d5b1409b734
Sandro Roger Boschetti <sandro.boschetti@ebserh.gov.br>
2022-09-13 17:36:15 -0300

    Nada demais. Só um teste.

--- src/main/java/srb/Main.java
+++ src/main/java/srb/Main.java
@@ -6,11 +6,12 @@ import srb.sendmsg.SendMsg;
 public class Main {
 
 	public static void main(String[] args) {
+		
 
 //		SendMsg.execute();
-//		ReadExcel.execute();
-		
+//		ReadExcel.execute();		
 		ReadExcel.getInstance().print();
+		
 	}
 
 }
